7019ff4462fbaef1e3b43aaf38e3665a09a2f58d
Saray Rada <SarayRadaReboso@outlook.es>
2023-03-20 23:56:44 +0100

    fix: fixing test with soecial characters

--- tests/Passwordchecker.test.ts
+++ tests/Passwordchecker.test.ts
@@ -1,53 +1,73 @@
 import { passwordChecker } from "../src/passwordCheckr";
 
 describe("passwordChecker", () => {
-    it("checks that is a valid password", () => {
-        const password = "As8;hjskdL";
+	it("checks that is a valid password", () => {
+		const password = "As8;hjskdL";
 
-        const check = passwordChecker(password);
+		const check = passwordChecker(password);
 
-        expect(check).toBeTruthy();
-    });
-    it("checks that isn't valid with less than 8 characters", () => {
-        const password = "As8;hjs";
+		expect(check).toBeTruthy();
+	});
+	it("checks that isn't valid with less than 8 characters", () => {
+		const password = "As8;hjs";
 
-        const check = passwordChecker(password);
+		const check = passwordChecker(password);
 
-        expect(check).toBeFalsy();
-    });
-    it("checks that isn't valid with more than 20 characters", () => {
-        const password = "As8;hjsAs8;hjsAs8;hjs";
+		expect(check).toBeFalsy();
+	});
+	it("checks that isn't valid with more than 20 characters", () => {
+		const password = "As8;hjsAs8;hjsAs8;hjs";
 
-        const check = passwordChecker(password);
+		const check = passwordChecker(password);
 
-        expect(check).toBeFalsy();
-    });
-    it("checks that isn't valid without upperCase", () => {
-        const password = "as8;hjskdl";
+		expect(check).toBeFalsy();
+	});
+	it("checks that isn't valid without upperCase", () => {
+		const password = "as8;hjskdl";
 
-        const check = passwordChecker(password);
+		const check = passwordChecker(password);
 
-        expect(check).toBeFalsy();
-    });
-    it("checks that isn't valid without lowerCase", () => {
-        const password = "AS8;HJSKDL";
+		expect(check).toBeFalsy();
+	});
+	it("checks that isn't valid without lowerCase", () => {
+		const password = "AS8;HJSKDL";
 
-        const check = passwordChecker(password);
+		const check = passwordChecker(password);
 
-        expect(check).toBeFalsy();
-    });
-    it("checks that isn't valid without a number", () => {
-        const password = "ASj;HJSKDL";
+		expect(check).toBeFalsy();
+	});
+	it("checks that isn't valid without a number", () => {
+		const password = "ASj;HJSKDL";
 
-        const check = passwordChecker(password);
+		const check = passwordChecker(password);
 
-        expect(check).toBeFalsy();
-    });
-    it("checks that isn't valid without a special character", () => {
-        const password = "ASjmHJSK2L";
+		expect(check).toBeFalsy();
+	});
+	test.each([
+		";",
+		"(",
+		")",
+		"[",
+		"]",
+		"{",
+		"}",
+		":",
+		'"',
+		"'",
+		"?",
+		"!",
+		"*",
+		"=",
+		"+",
+		"-",
+		"_",
+		"¿",
+		"¡",
+	])(`checks that is valid with a special character`, (char: string) => {
+		const password = `ASjm${char}HJSK2L`;
 
-        const check = passwordChecker(password);
+		const check = passwordChecker(password);
 
-        expect(check).toBeFalsy();
-    });
+		expect(check).toBeTruthy();
+	});
 });
